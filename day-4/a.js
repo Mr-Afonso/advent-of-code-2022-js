493d274a4f62f1397b805954dedafdd3a7d61719
Miguel Afonso <miguel@g2g-online.nl>
2022-12-06 10:05:08 +0100

    Day 4

--- day-4/a.js
+++ day-4/a.js
@@ -1,3 +1,21 @@
 const input = await Deno.readTextFile("./input.txt")
 
-input.split('\n')
+let count = 0
+const cleanInput = input.split('\n').map((e) => {
+  return e.split(/,|-/)
+})
+
+cleanInput.map((space) => {
+  if (Number(space[2]) >= Number(space[0]) && Number(space[3]) <= Number(space[1])) {
+    count++
+  }
+
+  if ((Number(space[0]) > Number(space[2]) && Number(space[1]) < Number(space[3])) ||
+    (Number(space[0]) >= Number(space[2]) && Number(space[1]) < Number(space[3])) ||
+    (Number(space[0]) > Number(space[2]) && Number(space[1]) <= Number(space[3]))) {
+    count++
+  }
+
+})
+
+console.log(count)
---
+++ day-4/b.js
@@ -0,0 +1,19 @@
+const input = await Deno.readTextFile("./input.txt")
+
+let count = 0
+const cleanInput = input.split('\n').map((e) => {
+  return e.split(/,|-/)
+})
+
+cleanInput.map((space) => {
+  if (
+    (Number(space[1]) >= Number(space[2]) &&
+      Number(space[0]) <= Number(space[3])
+    )
+  ) {
+    count++
+  }
+
+})
+
+console.log(count)
